24cff043622f6522aae93eedd8e15e16de4991b3
Reeza Khadgi <khadgireeza@gmail.com>
2024-10-20 16:07:01 +0545

    About page added

--- Second_Project/Controllers/HomeController.cs
+++ Second_Project/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Second_Project.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Second_Project/obj/Debug/net8.0/Second_Project.AssemblyInfo.cs
+++ Second_Project/obj/Debug/net8.0/Second_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Second_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c5aac49ded2839ce1f725a3ec44125cca49fbae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Second_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Second_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
