f3f32e4f59105b82f0f54182105582a0007961ee
Reeza Khadgi <khadgireeza@gmail.com>
2024-10-21 20:33:21 +0545

    login created

---
+++ Second_Project/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Second_Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // This will return the Index.cshtml view in Views/Login/
+        }
+
+        // Handle form submission
+        [HttpPost]
+        public IActionResult Authenticate(string username, string password)
+        {
+            // Authentication logic here
+            if (username == "admin" && password == "password")
+            {
+                return RedirectToAction("Index", "Home"); // Redirect to Home if login is successful
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid login credentials.";
+                return View("Index"); // Stay on the same page with an error message
+            }
+        }
+    }
+}
